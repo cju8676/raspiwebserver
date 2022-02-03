254abadc26a664eedea79951db0bde455312db6a
Corey Urbanke <cju8676@rit.edu>
2022-02-03 17:34:15 -0500

    cont. cleanup of SettingsPage - each component handles their own submit now - each keeps their own is blank state

--- src/EditForm.js
+++ src/EditForm.js
@@ -1,11 +1,47 @@
 import { React, Component } from "react"
-import { Header, Button, Divider, Confirm, Container, Segment, Input } from 'semantic-ui-react'
+import { Button, Segment, Input } from 'semantic-ui-react'
 
 class EditForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            blank : false
+        };
+        this.updateInfo = "";
+    }
 
+    handleUpdate = (e) => {
+        this.props.update(e, this.updateInfo);
+        this.updateInfo = "";
+        this.props.toggle();
+    }
+
+    submitUpdate = () => {
+        if (this.props.visible && this.updateInfo === "") {
+            this.setState({blank : true });
+            return;
+        }
+        const data = {
+            username : this.props.user,
+            new_name : this.updateInfo
+        }
+        const reqOptions = {
+            method : 'POST',
+            headers : { Accept : 'application/json', 'Content-Type' : 'application/json'},
+            body : JSON.stringify(data)
+        }
+        fetch(`/update${this.props.name}/`, reqOptions).then(res => res.json())
+            .then(
+                //todo handle success pop up
+                this.handleUpdate(this.props.name)
+            )
+
+    }
+
+    update = (event) => {
+        this.updateInfo = event.target.value;
+        if (event.target.value !== "") {
+            this.setState({blank : false})
         }
     }
 
@@ -15,9 +51,9 @@ class EditForm extends Component {
             <div>
                 <Segment>
                     <h4>Edit {this.props.name}</h4>
-                    <Input type='text' id={'entered'+this.props.name} placeholder={this.props.name} onChange={this.props.onChange} error={this.props.error} />
+                    <Input type='text' id={'entered'+this.props.name} placeholder={this.props.name} onChange={this.update} error={this.state.blank} />
                     <Button color='black' onClick={this.props.toggle}>Cancel</Button>
-                    <Button positive onClick={this.props.update}>Confirm</Button>
+                    <Button positive onClick={this.submitUpdate}>Confirm</Button>
                 </Segment>
             </div>
         )
--- src/SettingsPage.js
+++ src/SettingsPage.js
@@ -8,18 +8,15 @@ class SettingsPage extends Component {
         super(props);
         this.state = {
             open: false,
-            name : props.name,
-            user : props.user,
+            name: props.name,
+            user: props.user,
             logout: props.onChange,
             nameModal: false,
-            usernameModal : false,
-            passModal: false,
-            nameBlank: false,
-            userBlank : false,
-            passBlank: false
+            usernameModal: false,
+            passModal: false
         }
 
-        this.updateInfo= "";
+        this.updateInfo = "";
     }
 
     open = () => this.setState({ open: true })
@@ -39,73 +36,18 @@ class SettingsPage extends Component {
                 JSONresponse ? this.state.logout() : console.log("not del"))
     }
 
-    handleUpdate = (e) => {
-        if (e ==='Name') {
-            this.setState({name : this.updateInfo});
-            this.updateInfo= "";
-            this.toggleName();
-            this.componentDidMount();
-        }
-        else if (e === 'Username') {
-            this.setState({user : this.updateInfo});
-            this.updateInfo= "";
-            this.toggleuserName();
-            this.componentDidMount();
-        }
-        else if (e === 'Pass') {
-            //this.setState({})
-            this.updateInfo = "";
-            this.togglePass();
-            this.componentDidMount();
-        }
-    }
-
-    /* Update name or username in DB */
-    updateNameInfo = () => {
-        console.log(this.updateInfo)
-        if (this.state.nameModal && this.updateInfo === "") {
-            this.setState({nameBlank: true})
-            return;
-        }
-        else if (this.state.usernameModal && this.updateInfo === "") {
-            this.setState({userBlank : true})
-            return;
-        }
-        else if (this.state.passModal && this.updateInfo === "") {
-            this.setState({passBlank : true})
-            return;
-        }
-        const data = {
-            username: this.props.user,
-            new_name: this.updateInfo
-        }
-        const reqOptions = {
-            method: 'POST',
-            headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
-            body: JSON.stringify(data)
-        }
-        let update;
-        if (this.state.nameModal) update = 'Name'
-        else if (this.state.usernameModal) update = 'Username';
-        else update = 'Pass'
-        fetch('/update' + update + '/', reqOptions).then(response => response.json)
-            .then(
-                // todo handle success pop up
-                this.handleUpdate(update)
-            )
-    }
 
     toggleName = () => {
         // if off
         if (!this.state.nameModal) {
             // toggle name visibility, toggle everything else off
             this.setState({
-                nameModal : true,
-                usernameModal : false,
-                passModal : false
+                nameModal: true,
+                usernameModal: false,
+                passModal: false
             })
         }
-        else this.setState({nameModal : false})
+        else this.setState({ nameModal: false })
     }
 
     toggleuserName = () => {
@@ -113,12 +55,12 @@ class SettingsPage extends Component {
         if (!this.state.usernameModal) {
             // toggle user visibility, toggle everything else off
             this.setState({
-                usernameModal : true,
-                nameModal : false,
-                passModal : false
+                usernameModal: true,
+                nameModal: false,
+                passModal: false
             })
         }
-        else this.setState({usernameModal : false})
+        else this.setState({ usernameModal: false })
     }
 
     togglePass = () => {
@@ -126,28 +68,22 @@ class SettingsPage extends Component {
         if (!this.state.passModal) {
             // toggle pass visibility, toggle everything else off
             this.setState({
-                passModal : true,
-                nameModal : false,
-                usernameModal : false
+                passModal: true,
+                nameModal: false,
+                usernameModal: false
             })
         }
-        else this.setState({passModal : false})
+        else this.setState({ passModal: false })
     }
 
-    update = (event) => {
-        this.updateInfo = event.target.value;
-        if (event.target.value !== "") {
-            this.setState ({
-                nameBlank : false,
-                userBlank : false,
-                passBlank : false
-            })
-        }
+    update = (e, val) => {
+        if (e === 'Name') this.setState({ name: val })
+        else if (e === 'Username') this.setState({ user: val })
+        //else todo password
+        this.componentDidMount();
     }
 
-    componentDidMount () {
-
-    }
+    componentDidMount() { }
 
     render() {
         return (
@@ -158,39 +94,36 @@ class SettingsPage extends Component {
                 </Header>
                 <Container>
                     <h2>Name</h2>
-                    {this.state.name} <Button basic compact icon='edit' color='black' onClick={this.toggleName}/>
+                    {this.state.name} <Button basic compact icon='edit' color='black' onClick={this.toggleName} />
                     {this.state.nameModal && (
-                    <EditForm
-                        name="Name"
-                        visible= {this.state.nameModal}
-                        toggle={this.toggleName}
-                        update={this.updateNameInfo}
-                        error={this.state.nameBlank}
-                        onChange={this.update}
-                     />)}
+                        <EditForm
+                            user={this.state.user}
+                            name="Name"
+                            visible={this.state.nameModal}
+                            toggle={this.toggleName}
+                            update={this.update}
+                        />
+                    )}
                     <h2>Username</h2>
-                    {this.state.user} <Button basic compact icon='edit' color='black' onClick={this.toggleuserName}/>
+                    {this.state.user} <Button basic compact icon='edit' color='black' onClick={this.toggleuserName} />
                     {this.state.usernameModal && (
-                    <EditForm 
-                        name="Username" 
-                        visible={this.state.usernameModal}
-                        toggle={this.toggleuserName}
-                        update={this.updateNameInfo}
-                        error={this.state.userBlank}
-                        onChange={this.update}
-                    />
+                        <EditForm
+                            user={this.state.user}
+                            name="Username"
+                            visible={this.state.usernameModal}
+                            toggle={this.toggleuserName}
+                            update={this.update}
+                        />
                     )}
                     <h2>Password</h2>
-                    <Button basic compact icon='edit' color='black' onClick={this.togglePass}/>
+                    <Button basic compact icon='edit' color='black' onClick={this.togglePass} />
                     {this.state.passModal && (
-                        <EditForm 
-                        name="Password" 
-                        visible={this.state.passModal}
-                        toggle={this.togglePass}
-                        update={this.updateNameInfo}
-                        error={this.state.passBlank}
-                        onChange={this.update}
-
+                        <EditForm
+                            user={this.state.user}
+                            name="Password"
+                            visible={this.state.passModal}
+                            toggle={this.togglePass}
+                            update={this.update}
                         />
                     )}
                     <Divider />
